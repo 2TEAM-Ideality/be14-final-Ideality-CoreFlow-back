684756a34185a4eab8a589a20b962a9dfa8e97c4
rainyday1367 <rainyday1367@naver.com>
2025-05-22 12:14:15 +0900

    setting 백엔드 프로젝트 생성 완료

---
+++ src/main/java/com/ideality/coreflow/CoreflowBeApplication.java
@@ -0,0 +1,13 @@
+package com.ideality.coreflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreflowBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreflowBeApplication.class, args);
+    }
+
+}
