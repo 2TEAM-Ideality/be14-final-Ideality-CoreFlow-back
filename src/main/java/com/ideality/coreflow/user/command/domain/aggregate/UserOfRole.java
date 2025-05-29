30a5d06a8d6ddd701d85b414133f66eeafcccf51
swjang7269 <azamiyuko7269@gmail.com>
2025-05-29 09:40:02 +0900

    setting 유저, erp 관련 entity 생성

---
+++ src/main/java/com/ideality/coreflow/user/command/domain/aggregate/UserOfRole.java
@@ -0,0 +1,26 @@
+package com.ideality.coreflow.user.command.domain.aggregate;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "user_of_role")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserOfRole {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(name = "role_id", nullable = false)
+    private Long roleId;
+}
