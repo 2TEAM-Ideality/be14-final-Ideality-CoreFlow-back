960aa9af72e43a8730e88130322099bfaac23748
mins00kwon <mskwon0918@gmail.com>
2025-06-01 16:07:18 +0900

    feat: 단순 프로젝트 생성 기능 구현 완료 - Project Entity 수정 (template 컬럼 자료형을 Template->Long)

--- src/main/java/com/ideality/coreflow/project/command/controller/ProjectController.java
+++ src/main/java/com/ideality/coreflow/project/command/controller/ProjectController.java
@@ -1,5 +1,14 @@
 package com.ideality.coreflow.project.command.controller;
 
+import com.ideality.coreflow.project.command.domain.aggregate.Project;
+import com.ideality.coreflow.project.command.dto.ProjectCreateRequest;
+import com.ideality.coreflow.project.command.dto.ProjectCreateResponse;
+import com.ideality.coreflow.project.command.service.ProjectService;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +19,16 @@ import lombok.RequiredArgsConstructor;
 @RestController
 public class ProjectController {
 
-	// 템플릿 선택해서 프로젝트 생성
+    private final ProjectService projectService;
 
+    @PostMapping
+    public ResponseEntity<Map<String, Object>> createProject(@RequestBody ProjectCreateRequest request) {
+        Project result = projectService.createProject(request);
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", "success");
+        response.put("message", result.getId()+"번 프로젝트 생성 완료");
+        response.put("data", result);
+        return ResponseEntity.ok(response);
+    }
 
 }
