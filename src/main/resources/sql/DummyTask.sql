b70aa0bfacd83627dcb6d72bfe198508f4f22d3a
Hailyee <a11roundworld@naver.com>
2025-06-04 10:52:22 +0900

    더미데이터수정

--- src/main/resources/sql/DummyTask.sql
+++ src/main/resources/sql/DummyTask.sql
@@ -1,3 +1,5 @@
+USE company_a;
+
 INSERT INTO work
 (name, description, created_at, start_base, end_base, start_expect, end_expect, project_id)
 VALUES
@@ -40,7 +42,15 @@ VALUES
  "2025-06-11",
  "2025-06-13",
  1
-);
+) ;
+
+-- 원부자재 발주 태스크에 대한 세부일정 더미데이터 넣기
+INSERT INTO work (name, description, created_at, start_base, end_base, start_expect, end_expect, parent_task_id, project_id)
+VALUES
+    ('세부일정 1', '원부자재 발주에 대한 세부일정1', CURRENT_TIMESTAMP, '2025-01-01', '2025-01-10', '2025-01-01', '2025-01-10',  4, 1),    -- parent_task_id = 4
+    ('세부일정 2', '원부자재 발주에 대한 세부일정2', CURRENT_TIMESTAMP, '2025-01-01', '2025-01-10', '2025-01-01', '2025-01-10',  4, 1),    -- parent_task_id = 4
+    ('세부일정 3', '원부자재 발주에 대한 세부일정3', CURRENT_TIMESTAMP, '2025-01-01', '2025-01-10', '2025-01-01', '2025-01-10',  4, 1);    -- parent_task_id = 4
+
 
 -- 작업 별 참여 부서
 INSERT INTO work_dept
@@ -53,8 +63,11 @@ VALUES
 (
  2,
  1
-),2
-    ),
+),
+(
+    2,
+    2
+),
 (
     2,
     3
@@ -70,9 +83,22 @@ VALUES
 (
  4,
  4
-);
+),
 (
-    2,
+    5,
+    1
+),
+(
+    6,
+    2
+),
+(
+    7,
+    3
+)
+;
+
+
 
 
 -- 작업 별 관계
@@ -90,7 +116,13 @@ VALUES
 (
  2,
  4
-);
+),
+(5,
+ 6
+),
+(6,
+ 7)
+;
 
 -- 도식화 (work.id = 1)
 INSERT INTO participant (target_type, target_id, user_id, role_id)
@@ -111,6 +143,16 @@ VALUES
     ('TASK', 3, 4, 3),
     -- 원부자재 발주 (work.id = 4)
     ('TASK', 4, 7, 2),
-    ('TASK', 4, 8, 2);
+    ('TASK', 4, 8, 2),
+    -- 세부일정 1(work.id = 5)
+    ('DETAILED', 5, 7, 6),
+    ('DETAILED', 5, 8, 7),
+    -- 세부일정 2(work.id = 6)
+    ('DETAILED', 6, 8, 6),
+    ('DETAILED', 6, 7, 7),
+    -- 세부일정 3(work.id = 7)
+    ('DETAILED', 7, 7, 6),
+    ('DETAILED', 7, 8, 7);
+
 
 
