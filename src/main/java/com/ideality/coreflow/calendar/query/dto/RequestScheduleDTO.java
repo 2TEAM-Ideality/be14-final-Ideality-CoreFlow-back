0fb07c455b495b0ab5313ab3e34a761a48b2d3aa
minsun24 <jms49526@gmail.com>
2025-06-05 09:05:49 +0900

    FEAT : 개인 일정 생성 기능 구현 시작

--- src/main/java/com/ideality/coreflow/calendar/query/dto/RequestScheduleDTO.java
+++ src/main/java/com/ideality/coreflow/calendar/query/dto/RequestScheduleDTO.java
@@ -1,5 +1,7 @@
 package com.ideality.coreflow.calendar.query.dto;
 
+import java.time.LocalDateTime;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,6 +10,10 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class RequestScheduleDTO {
+	private String name;
+	private String description;
+	private LocalDateTime startDate;
+	private LocalDateTime endDate;
+
 }
