45d0545473dec1aa1fe4360c4b87aa73976a2e4a
mins00kwon <mskwon0918@gmail.com>
2025-06-06 19:09:12 +0900

    fix: service -> FacadeService 수정 완료

--- src/main/java/com/ideality/coreflow/project/query/controller/ProjectQueryController.java
+++ src/main/java/com/ideality/coreflow/project/query/controller/ProjectQueryController.java
@@ -3,6 +3,7 @@ package com.ideality.coreflow.project.query.controller;
 import com.ideality.coreflow.common.response.APIResponse;
 import com.ideality.coreflow.project.query.dto.ProjectSummaryDTO;
 import com.ideality.coreflow.project.query.service.ProjectQueryService;
+import com.ideality.coreflow.project.query.service.facade.ProjectQueryFacadeService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,11 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class ProjectQueryController {
 
-    private final ProjectQueryService projectQueryService;
+    private final ProjectQueryFacadeService projectQueryFacadeService;
 
     @GetMapping("/list") // 임시로 userId를 param으로 받아옴. 추후 반드시 수정
     public APIResponse<List<ProjectSummaryDTO>> getProjects(@RequestParam Long userId) {
-        List<ProjectSummaryDTO> projects = projectQueryService.selectProjectSummaries(userId);
+        List<ProjectSummaryDTO> projects = projectQueryFacadeService.selectProjectSummaries(userId);
         int count=projects.size();
         return APIResponse.success(projects, "참여중인 프로젝트 목록 조회 완료 ("+count+"개)");
     }
