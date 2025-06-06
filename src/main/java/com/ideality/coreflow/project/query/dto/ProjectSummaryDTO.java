327796f466cb81de8fa978a718a6508b53620806
mins00kwon <mskwon0918@gmail.com>
2025-06-06 15:36:39 +0900

    feat: 프로젝트 목록 조회 구현중... - Mapper 구현 완료 (1차)

---
+++ src/main/java/com/ideality/coreflow/project/query/dto/ProjectSummaryDTO.java
@@ -0,0 +1,16 @@
+package com.ideality.coreflow.project.query.dto;
+
+
+import com.ideality.coreflow.approval.domain.aggregate.Status;
+
+public class ProjectSummaryDTO {
+    private String name;
+    private Status status;
+    private String startDate;
+    private String endDate;
+    private DirectorSummaryDTO director;
+    private Double progressRate;
+    private Double passedRate;
+    private Integer delayDays;
+
+}
