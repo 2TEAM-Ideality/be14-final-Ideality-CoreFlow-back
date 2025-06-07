cee53aeb9c6cbef74f074054867870833dbf49c5
mins00kwon <mskwon0918@gmail.com>
2025-06-07 17:35:07 +0900

    feature: 프로젝트 상세 조회 구현 완료

--- src/main/java/com/ideality/coreflow/project/query/service/ProjectQueryService.java
+++ src/main/java/com/ideality/coreflow/project/query/service/ProjectQueryService.java
@@ -1,8 +1,11 @@
 package com.ideality.coreflow.project.query.service;
 
+import com.ideality.coreflow.project.query.dto.ProjectDetailResponseDTO;
 import com.ideality.coreflow.project.query.dto.ProjectSummaryDTO;
 import java.util.List;
 
 public interface ProjectQueryService {
     List<ProjectSummaryDTO> selectProjectSummaries(Long userId);
+
+    ProjectDetailResponseDTO getProjectDetail(Long projectId);
 }
