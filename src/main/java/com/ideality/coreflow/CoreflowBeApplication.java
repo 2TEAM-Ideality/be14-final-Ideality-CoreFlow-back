ae125e0caa68c8ba4feb21df7df240c0b4cfc1c4
rainyday1367 <rainyday1367@naver.com>
2025-06-22 18:37:18 +0900

    chore 설정 변경(오류 발생)

--- src/main/java/com/ideality/coreflow/CoreflowBeApplication.java
+++ src/main/java/com/ideality/coreflow/CoreflowBeApplication.java
@@ -7,7 +7,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class CoreflowBeApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CoreflowBeApplication.class, args);
     }
