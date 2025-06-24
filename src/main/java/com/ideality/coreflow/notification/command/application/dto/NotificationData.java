08be90b18b787de54137485cc16e0571389c8373
Hailyee <a11roundworld@naver.com>
2025-06-24 12:03:31 +0900

    알ㄹ림

--- src/main/java/com/ideality/coreflow/notification/command/application/dto/NotificationData.java
+++ src/main/java/com/ideality/coreflow/notification/command/application/dto/NotificationData.java
@@ -1,6 +1,7 @@
 package com.ideality.coreflow.notification.command.application.dto;
 
 import com.ideality.coreflow.notification.command.domain.aggregate.Status;
+import com.ideality.coreflow.notification.command.domain.aggregate.TargetType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
@@ -17,15 +18,19 @@ public class NotificationData {
     private final String status;
     private final Long id;  // ID 필드 추가
     private final Boolean isAutoDelete;
+    private final Long targetId;
+    private final TargetType targetType;
 
     // 생성자
     // 생성자: LocalDateTime과 Status를 String으로 변환
-    public NotificationData(String content, LocalDateTime date, Status status, Long id, Boolean isAutoDelete) {
+    public NotificationData(String content, LocalDateTime date, Status status, Long id, Boolean isAutoDelete,Long targetId,TargetType targetType) {
         this.content = content;
         this.date = date.toString();  // LocalDateTime을 String으로 변환
         this.status = status.toString();  // Status를 String으로 변환
         this.id = id;
         this.isAutoDelete = isAutoDelete;
+        this.targetId = targetId;  // targetId 필드 초기화
+        this.targetType = targetType;
     }
 
 }
