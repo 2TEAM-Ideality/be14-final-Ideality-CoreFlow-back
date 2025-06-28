940b6255751aef8bdec0776551108e0d4569a10b
swjang7269 <azamiyuko7269@gmail.com>
2025-06-28 19:45:05 +0900

    refactor null 검사 추가

--- src/main/java/com/ideality/coreflow/project/command/domain/service/WorkDomainServiceImpl.java
+++ src/main/java/com/ideality/coreflow/project/command/domain/service/WorkDomainServiceImpl.java
@@ -64,6 +64,7 @@ public class WorkDomainServiceImpl implements WorkDomainService {
     // 두 기간 사이의 워킹데이 계산
     @Override
     public Integer calculateWorkingDutarion(LocalDate startReal, LocalDate endReal) {
+        if (startReal == null || endReal == null) return null;
         long totalDuration = ChronoUnit.DAYS.between(startReal, endReal) + 1;
         int holidays = holidayQueryService.countHolidaysBetween(startReal, endReal);
         return (int)(totalDuration - holidays);
