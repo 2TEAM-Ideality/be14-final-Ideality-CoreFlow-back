025e36796f5ffc194e495f8b2d44061c9adae9ba
mins00kwon <mskwon0918@gmail.com>
2025-06-28 16:49:24 +0900

    fix: warning 관련 코드 수정

--- src/main/java/com/ideality/coreflow/project/command/application/service/facade/ProjectFacadeService.java
+++ src/main/java/com/ideality/coreflow/project/command/application/service/facade/ProjectFacadeService.java
@@ -485,6 +485,9 @@ public class ProjectFacadeService {
         Long detailId = detailService.createDetail(requestDetailDTO);
         log.info("세부 일정 생성");
 
+        // 새로 생성된 세부일정이 태스크의 범위를 넘는다면 태스크를 warning 상태로 업데이트
+        updateTaskWarning(requestDetailDTO.getParentTaskId());
+
         //1. source와 target 모두 null일 경우, 관계 설정을 생략
         if ((requestDetailDTO.getSource() == null || requestDetailDTO.getSource().isEmpty()) &&
                 (requestDetailDTO.getTarget() == null || requestDetailDTO.getTarget().isEmpty())) {
